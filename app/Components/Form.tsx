daa315e82ef77442e70e7d3ed14f647a070c7772
[MxoMasuku] <64246766+mxomasuku@users.noreply.github.com>
2023-10-30 01:29:46 +0200

    newsletter sign up fix

--- app/Components/Form.tsx
+++ app/Components/Form.tsx
@@ -4,7 +4,8 @@ import Image from "next/image"
 const Form = () => {
   return (
 
-            <form className='px-8 md:py-24 my-24 lg:mx-24 bg-gray-600 text-center text-white'>
+            <div className='px-8 md:py-24 my-24 lg:mx-24 bg-gray-600 text-center text-white'>
+
               <div className="text-center">
                 <div className="mx-auto">
                 <Image
@@ -20,14 +21,17 @@ const Form = () => {
                     <h3 className="text-3xl text-center">Subscribe to our newsletter</h3>
                     <p>Get monthly updates about Humanism to your inbox</p>
                
-                    <input type={'email'} placeholder={'enter your email'} className="md:w-96 bg-gray-100 p-2 rounded-md inline-block" />
+                    {/* <input type={'email'} placeholder={'enter your email'} className="md:w-96 bg-gray-100 p-2 rounded-md inline-block" /> */}
                
-                <button type={'submit'} className="rounded-md mx-2 mb-4 py-2 px-8 text-white rounded-md bg-gradient-to-r from-pink-600 to-yellow-400 mt-4 hover:bg-black ">Subscribe</button>
-            </form>
+             <a href="https://docs.google.com/forms/d/e/1FAIpQLSfIRCHbKzS4dRhnYp2xCXTJF9Ra-CADU2g0Iso8nq8em4VSiQ/viewform?usp=sf_link" target="_blank"><button className="rounded-md mx-2 mb-4 py-2 px-8 text-white rounded-md bg-gradient-to-r from-pink-600 to-yellow-400 mt-4 hover:bg-black ">Subscribe</button></a>
+            </div>
 
 
 
   )
 }
+
+
+
 
 export default Form
--- app/about/yhz/page.tsx
+++ app/about/yhz/page.tsx
@@ -2,10 +2,13 @@ import Image from "next/image"
 const about = () => {
   return (
     <>
-    <div className="text-center mt-36 ">
+    <div className="text-center mt-36 md:mx-24 ">
         <h1 className='text-4xl font-bold text-white bg-gradient-to-r from-pink-600  to-yellow-400 my-4 py-10 lg:text-center lg:mx-32 rounded-lg'>Young Humanists Zimbabwe</h1>
-        <h3>Humanism offers the youth an exciting chance to connect and experience the vibrancy of their lives and lead more meaningful lives</h3>
+        <h3 className="font-semibold text-gray-800 text-md">Humanism offers the youth an exciting chance to connect and experience the vibrancy of their lives and lead more meaningful lives</h3>
     </div>
+
+<a href="https://docs.google.com/forms/d/e/1FAIpQLScRiGLg1T6SSSlDb8R-uB00Gg6SiBGy-ni0UDKUYikl66vvFw/viewform?usp=sf_link" target="_blank" className="text-center"><button type={'submit'} className="rounded-md mx-2 mb-4 py-2 px-8 text-white rounded-md bg-gradient-to-r from-pink-600 to-yellow-400 mt-4 hover:bg-black ">Sign Up</button></a>
+  
     <div className={'text-center'}>
     <Image
     src={'/StaticResources/yhzelement.jpg'}
